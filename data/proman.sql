076e4c1512dbb996d8c839d46749c8d361379088
Szucs Nikolett <lexi.nikolett.szucs@gmail.com>
2019-09-16 18:50:44 +0200

    fix create/insert order

--- data/proman.sql
+++ data/proman.sql
@@ -22,21 +22,6 @@ INSERT INTO boards VALUES (2, 'Board 2');
 SELECT pg_catalog.setval('boards_id_seq', 2, true);
 
 
-INSERT INTO cards VALUES (1, 1, 'new card 1', 0, 0);
-INSERT INTO cards VALUES (2, 1, 'new card 2', 0, 1);
-INSERT INTO cards VALUES (3, 1, 'in progress card' ,1, 0);
-INSERT INTO cards VALUES (4, 1, 'planning',2 ,0);
-INSERT INTO cards VALUES (5, 1, 'done card 1', 3, 0);
-INSERT INTO cards VALUES (6, 1, 'done card 1', 3, 1);
-INSERT INTO cards VALUES (7, 2, 'new card 1', 0, 0);
-INSERT INTO cards VALUES (8 ,2, 'new card 2', 0, 1);
-INSERT INTO cards VALUES (9, 2, 'in progress card', 1, 0);
-INSERT INTO cards VALUES (10, 2, 'planning', 2, 0);
-INSERT INTO cards VALUES (11, 2, 'done card 1', 3, 0);
-INSERT INTO cards VALUES (12, 2, 'done card 1', 3, 1);
-SELECT pg_catalog.setval('cards_id_seq', 12, true);
-
-
 DROP TABLE IF EXISTS public.statuses CASCADE;
 DROP SEQUENCE IF EXISTS public.statuses_id_seq;
 CREATE TABLE statuses (
@@ -59,3 +44,16 @@ CREATE TABLE cards (
     status_id int REFERENCES statuses(id),
     order_num int
 );
+INSERT INTO cards VALUES (1, 1, 'new card 1', 0, 0);
+INSERT INTO cards VALUES (2, 1, 'new card 2', 0, 1);
+INSERT INTO cards VALUES (3, 1, 'in progress card' ,1, 0);
+INSERT INTO cards VALUES (4, 1, 'planning',2 ,0);
+INSERT INTO cards VALUES (5, 1, 'done card 1', 3, 0);
+INSERT INTO cards VALUES (6, 1, 'done card 1', 3, 1);
+INSERT INTO cards VALUES (7, 2, 'new card 1', 0, 0);
+INSERT INTO cards VALUES (8 ,2, 'new card 2', 0, 1);
+INSERT INTO cards VALUES (9, 2, 'in progress card', 1, 0);
+INSERT INTO cards VALUES (10, 2, 'planning', 2, 0);
+INSERT INTO cards VALUES (11, 2, 'done card 1', 3, 0);
+INSERT INTO cards VALUES (12, 2, 'done card 1', 3, 1);
+SELECT pg_catalog.setval('cards_id_seq', 12, true);
